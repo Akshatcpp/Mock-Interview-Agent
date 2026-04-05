c160051933f843a96a3a612b67b58f0986e71719
Akshat Raikhere <raikhereakshat@gmail.com>
2026-04-05 19:14:56 +0530

    Add Vercel Speed Insights for performance monitoring

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         {children}
         <Toaster/>
+        <SpeedInsights />
       </body>
     </html>
     </ClerkProvider>
